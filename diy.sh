b828b65b71b4f716c1be462d171039baa8f38084
pkpkgtr1 <48145298+pkpkgtr1@users.noreply.github.com>
2020-03-04 17:30:38 +0800

    Update

--- diy.sh
+++ diy.sh
@@ -8,4 +8,9 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/tzxiaozhen88/luci-app-koolproxyR.git package/luci-app-koolproxyR
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/honwen/luci-app-aliddns.git package/luci-app-aliddns
+git clone https://github.com/Apocalypsor/luci-app-smartdns.git package/luci-app-smartdns
 git clone https://github.com/Lienol/openwrt-package.git package/Lienol
+
+
