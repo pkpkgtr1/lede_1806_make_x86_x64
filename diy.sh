6bbf5f8eb55ba62ae2d5699683a1d41afa682b24
pkpkgtr1 <48145298+pkpkgtr1@users.noreply.github.com>
2020-03-04 14:22:47 +0800

    Update

--- diy.sh
+++ diy.sh
@@ -7,5 +7,5 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-git clone https://github.com/tzxiaozhen88/luci-app-koolproxyR.git package/pkpkgtr/luci-app-koolproxyR
+git clone https://github.com/tzxiaozhen88/luci-app-koolproxyR.git package/luci-app-koolproxyR
 git clone https://github.com/Lienol/openwrt-package.git package/Lienol
