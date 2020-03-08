f9df63ab4b4ce21eee77c8eaa8f8e5cebaa8dc91
pkpkgtr1 <48145298+pkpkgtr1@users.noreply.github.com>
2020-03-08 12:53:56 +0800

    add luci-app-clash

--- diy.sh
+++ diy.sh
@@ -12,6 +12,7 @@ git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci
 git clone https://github.com/honwen/luci-app-aliddns.git package/luci-app-aliddns
 #git clone https://github.com/Apocalypsor/luci-app-smartdns.git package/luci-app-smartdns
 git clone https://github.com/Lienol/openwrt-package.git package/Lienol
+git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
 #================================================= 主题
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
 
