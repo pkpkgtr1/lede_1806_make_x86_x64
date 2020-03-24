8d930ed5d0b912b6dafd8b512d712d943920f92d
pkpkgtr1 <48145298+pkpkgtr1@users.noreply.github.com>
2020-03-24 10:02:58 +0800

    add luci-app-advancedsetting

--- diy.sh
+++ diy.sh
@@ -13,6 +13,7 @@ git clone https://github.com/honwen/luci-app-aliddns.git package/luci-app-aliddn
 #git clone https://github.com/Apocalypsor/luci-app-smartdns.git package/luci-app-smartdns
 git clone https://github.com/Lienol/openwrt-package.git package/Lienol
 git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
+svn co https://github.com/pkpkgtr1/pkpkgtr-openwrt-package/trunk/luci-app-advancedsetting package/luci-app-advancedsetting
 #================================================= 主题
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
 
