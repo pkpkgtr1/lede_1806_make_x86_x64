29fe9f10d2809268252e12108c05b4dba3c9a0e1
pkpkgtr1 <48145298+pkpkgtr1@users.noreply.github.com>
2020-04-28 17:42:21 +0800

    Build OpenWrt

--- diy.sh
+++ diy.sh
@@ -13,6 +13,7 @@ git clone https://github.com/honwen/luci-app-aliddns.git package/luci-app-aliddn
 #git clone https://github.com/Apocalypsor/luci-app-smartdns.git package/luci-app-smartdns
 git clone https://github.com/pkpkgtr1/openwrt-package.git package/Lienol
 git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
+git clone https://github.com/fw876/helloworld.git package/helloworld
 svn co https://github.com/pkpkgtr1/pkpkgtr-openwrt-package/trunk/luci-app-advancedsetting package/luci-app-advancedsetting
 
 #================================================= 主题
